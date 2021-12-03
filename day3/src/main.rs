3a95bdcea89e26e406bcad1bc181ddbbd4867ccc
Scott Riley <scott.riley111@gmail.com>
2021-12-03 15:39:49 +0000

    completes day3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,62 +1,97 @@
-use std::collections::HashMap;
-
-type BitValue = isize;
-
-const TWELVE_BIT_NUMBER: [i16; 12] = [2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1];
-
 fn main() {
     let s = include_str!("input.txt");
     let input: Vec<&str> = s.split('\n').filter(|line| line.len() > 1).collect();
 
-    let mut bit_counts: HashMap<BitValue, isize> = HashMap::new();
+    let mut possible_oxygen_values = input.clone();
 
-    for line in input {
-        for (bit, value) in line.chars().zip(TWELVE_BIT_NUMBER.iter()) {
-            match bit {
-                '0' => {
-                    *bit_counts.entry((*value).into()).or_insert(-1) -= 1;
-                }
-                '1' => {
-                    *bit_counts.entry((*value).into()).or_insert(1) += 1;
-                }
-                not_a_bit => panic!("Expected a 1 or 0, got: {:?}", not_a_bit),
-            }
+    for position in 0..12 {
+        // we could also partition and then just take largest, rather than find largest + filter
+        let bit = bit_occurrences(
+            &possible_oxygen_values,
+            position,
+            OccurrenceMode::MostCommon,
+        );
+
+        possible_oxygen_values =
+            filter_lines_with_bit(possible_oxygen_values, position, dbg!(bit).to_string());
+
+        if possible_oxygen_values.len() == 1 {
+            break;
         }
     }
 
-    let mut x: Vec<(&BitValue, &isize)> = bit_counts.iter().collect();
+    let mut possible_co2_values = input;
 
-    x.sort_by(|(x, _), (y, _)| y.partial_cmp(x).unwrap());
-
-    let mut gamma = String::new();
-    let mut epsilon = String::new();
-
-    x.iter().for_each(|(_, count)| {
-        gamma.push(to_gamma_bit(**count));
-        epsilon.push(to_epsilon_bit(**count));
-    });
-
-    let gamma = u32::from_str_radix(gamma.as_str(), 2).unwrap();
-    let epsilon = u32::from_str_radix(epsilon.as_str(), 2).unwrap();
+    for position in 0..12 {
+        // we could also partition and then just take largest, rather than find largest + filter
+        let bit = bit_occurrences(&possible_co2_values, position, OccurrenceMode::LeastCommon);
+        possible_co2_values =
+            filter_lines_with_bit(possible_co2_values, position, dbg!(bit).to_string());
+        if possible_co2_values.len() == 1 {
+            break;
+        }
+    }
 
     println!(
-        "gamma={} epsilon={} result={}",
-        gamma,
-        epsilon,
-        gamma * epsilon
+        "{:?}",
+        u32::from_str_radix(possible_oxygen_values.first().unwrap(), 2)
+    );
+
+    println!(
+        "{:?}",
+        u32::from_str_radix(possible_co2_values.first().unwrap(), 2)
     );
 }
 
-fn to_gamma_bit(val: isize) -> char {
-    match val > 0 {
-        true => '1',
-        false => '0',
+#[derive(Debug)]
+enum OccurrenceMode {
+    MostCommon,
+    LeastCommon,
+}
+
+impl OccurrenceMode {
+    pub fn for_val(&self, value: i32) -> char {
+        use OccurrenceMode::*;
+        if value == 0 {
+            return match self {
+                MostCommon => '1',
+                LeastCommon => '0',
+            };
+        }
+        match (self, value > 0) {
+            (MostCommon, true) | (LeastCommon, false) => '1',
+            (MostCommon, false) | (LeastCommon, true) => '0',
+        }
     }
 }
 
-fn to_epsilon_bit(val: isize) -> char {
-    match val > 0 {
-        true => '0',
-        false => '1',
+fn bit_occurrences(lines: &[&str], position: usize, mode: OccurrenceMode) -> char {
+    let mut counter = 0;
+
+    for line in lines {
+        match str_at(line, position).unwrap() {
+            "0" => {
+                counter -= 1;
+            }
+            "1" => {
+                counter += 1;
+            }
+            not_a_bit => panic!("Expected a 1 or 0, got: {:?}", not_a_bit),
+        }
     }
+
+    mode.for_val(counter)
+}
+
+// rewrite to avoid extra allocation/don't collect
+fn filter_lines_with_bit(lines: Vec<&str>, position: usize, bit: String) -> Vec<&str> {
+    lines
+        .iter()
+        .filter(|line| str_at(line, position) == Some(bit.as_str()))
+        .copied()
+        .collect()
+}
+
+fn str_at(string: &str, pos: usize) -> Option<&str> {
+    string.get(pos..(pos + 1))
 }

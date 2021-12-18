4cb636d753b565e0df97cb6ad8b4913519ed1149
Scott Riley <scott.riley111@gmail.com>
2021-12-18 21:44:23 +0000

    completes day10 part2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -5,34 +5,38 @@ fn main() {
 
     let lines: Vec<&str> = s.split('\n').collect();
 
-    let illegal_chars: Vec<char> = lines
+    let illegal_chars: Vec<Vec<char>> = lines
         .iter()
-        .filter_map(|line| first_illegal_char(line))
+        .filter_map(|line| remaining_required_chars(line))
         .collect();
 
-    println!("result={:?}", illegal_chars);
+    let mut scores: Vec<usize> = illegal_chars
+        .iter()
+        .map(|chars| chars.iter().rfold(0, |acc, char| (acc * 5) + score(*char)))
+        .collect();
 
-    let score: usize = illegal_chars.into_iter().map(score).sum();
-    println!("score={}", score)
+    scores.sort_unstable();
+
+    println!("result={}", scores[scores.len() / 2])
 }
 
 fn score(bracket: char) -> usize {
     match bracket {
-        ')' => 3,
-        ']' => 57,
-        '}' => 1197,
-        '>' => 25137,
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
         _ => panic!("unrecognised bracket {}", bracket),
     }
 }
 
-fn first_illegal_char(line: &str) -> Option<char> {
+fn remaining_required_chars(line: &str) -> Option<Vec<char>> {
     let mut bracket_stack: VecDeque<char> = VecDeque::new();
     for char in line.chars() {
         if bracket_stack.is_empty() {
             bracket_stack.push_back(char);
         } else if is_closing(char) && !can_close(char, *bracket_stack.back().unwrap()) {
-            return Some(char);
+            return None;
         } else if is_closing(char) && can_close(char, *bracket_stack.back().unwrap()) {
             bracket_stack.pop_back();
         } else {
@@ -40,7 +44,17 @@ fn first_illegal_char(line: &str) -> Option<char> {
         }
     }
 
-    None
+    if bracket_stack.is_empty() {
+        return None;
+    }
+
+    let mut completion = vec![];
+
+    for bracket in bracket_stack {
+        completion.push(matching(bracket));
+    }
+
+    Some(completion)
 }
 
 fn can_close(bracket: char, target: char) -> bool {
@@ -48,6 +62,16 @@ fn can_close(bracket: char, target: char) -> bool {
         (bracket, target),
         (']', '[') | (')', '(') | ('>', '<') | ('}', '{')
     )
+}
+
+fn matching(bracket: char) -> char {
+    match bracket {
+        '(' => ')',
+        '<' => '>',
+        '{' => '}',
+        '[' => ']',
+        _ => panic!("unkwown bracket type"),
+    }
 }
 
 fn is_closing(bracket: char) -> bool {
